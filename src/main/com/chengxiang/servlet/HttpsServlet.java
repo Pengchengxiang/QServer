3bd42c06f4a4e1f7aea647c3ffa747675d791a41
chengxiang.peng <chengxiang.peng@qunar.com>
2016-11-06 22:08:30 +0800

    do https

--- src/main/com/chengxiang/servlet/HttpsServlet.java
+++ src/main/com/chengxiang/servlet/HttpsServlet.java
@@ -1,10 +1,9 @@
-package main.com.chengxiang;
+package main.com.chengxiang.servlet;
 
 import com.alibaba.fastjson.JSON;
 import main.com.chengxiang.response.HttpsResponse;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +13,25 @@ import java.io.PrintStream;
 /**
  * Created by chengxiang.peng on 2016/11/4.
  */
-@WebServlet(name = "HttpsServlet", urlPatterns = {"/HttpsServlet"})
 public class HttpsServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req,resp);
+        System.out.println("doPost");
+        doLoginRequest(req, resp);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+        doLoginRequest(req, resp);
+    }
+
+    private void doLoginRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         PrintStream printStream = new PrintStream(resp.getOutputStream());
+        HttpsResponse httpsResponse = new HttpsResponse();
         String userName = req.getParameter("userName");
         String passWord = req.getParameter("passWord");
-        HttpsResponse httpsResponse = new HttpsResponse();
-        if ("123".equals(userName) && passWord.equals("123")) {
+        if ("123".equals(userName) && "123".equals(passWord)) {
             httpsResponse.setCode("000");
             httpsResponse.setMessage("login success!");
         } else {
